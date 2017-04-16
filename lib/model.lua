a8ea55b41ef707c8ac5a0a904e8a69530741c5a7
Macbook Air <macbookair@MacBook-Air-de-Macbook.local>
2017-04-15 23:52:40 -0300

    Sync with GIT

---
+++ lib/model.lua
@@ -0,0 +1,252 @@
+local sqlite3 = require( "sqlite3" )
+
+local path = system.pathForFile( "data.db", system.DocumentsDirectory )
+
+--[[local tablesetup = "CREATE TABLE IF NOT EXISTS test (id INTEGER PRIMARY KEY autoincrement, name, description, website);"
+db:exec( tablesetup )
+for row in db:nrows("SELECT * FROM test") do
+    print( "Row " .. row.id )
+ 
+    -- Create table at the next available array index
+    people[#people+1] =
+    {
+        name = row.name,
+        description = row.description,
+        website = row.website
+    }
+end]]
+DEBUG = false
+class = require("lib.external.30log")
+require("lib.table")
+require("lib.util.table_util")
+
+Model = class("Model")
+
+function Model:init(columns, model)
+
+    if columns == nil or type(columns) ~= "table" then
+        error("A model cannot be instantiated without column")
+    end
+    
+    if model == nil or type(model) ~= "table" then
+        error("Did you missed to pass your model?!")
+    end
+    
+    if model.columns == nil or type(model.columns) ~= "table" then
+        error("Did you missed to create your model columns definitions?")
+    end
+    -- percorrer a tabela
+    
+    -- save columns values in model.
+    for columnName, value in pairs(columns) do
+        if columnName ~= nil then              
+              model[columnName] = value
+        end
+    end
+    
+    self:_createTableSQL(model)
+end
+
+function Model:findAll()
+    local generatedSql = Table:_generateSQLSelect(self)
+    print(generatedSql)
+    -- reset clauses, so it can be set next time.
+    self.whereClause = nil
+    self.orClause = nil
+    --local rawResults = DBAdapter:querySQL(generatedSql)
+    
+    local models = self:buildModels(generatedSql)
+
+    return models
+end
+
+function Model:where(conditions)
+    self.whereClause = Table:_generateSQLWhere(conditions)
+    return self
+end
+
+function Model:OR(conditions)
+    if self.whereClause == nil or type(self.whereClause) ~= "string" then
+        error("You must call WHERE before calling or.")
+    end
+
+    self.orClause = Table:_generateSQLWhereOR(conditions)
+    return self
+end
+
+function Model:buildModels( SQL )
+    local db = sqlite3.open( path )
+    if SQL == nil then
+        error("Cannot generate models without SQL.")
+    end
+
+    if type(SQL) ~= "string" then
+        error("Cannot generate models without a valid raw sql.")
+    end
+
+    local models = {}
+    
+    for row in db:nrows(SQL) do
+        
+        local model = {}
+        local belongedModels = {}
+        local hasManyModels = {}
+
+        if self.belongsTo ~= nil then
+            for foreignKey, foreignTable in pairs(self.belongsTo) do
+
+                belongedModels[foreignKey] = {}
+            end
+        end
+
+        if self.hasMany ~= nil and type(self.hasMany) == "table" then
+            for foreignKey, foreignTable in pairs(self.hasMany) do
+
+                hasManyModels[foreignKey] = {}
+            end
+        end
+
+        for column, columnValue in pairs(row) do
+        
+            local columnNameWithAlias = column
+            
+            local columnNameWithoutAlias = string.gsub(columnNameWithAlias, "^(.*)_", "")
+            local alias = string.gsub(columnNameWithAlias, "_(.*)", "")
+            
+
+            if alias == self.name then
+                model[columnNameWithoutAlias] = columnValue
+            else
+                if self.belongsTo ~= nil and self.belongsTo[alias] ~= nil then
+                    belongedModels[alias][columnNameWithoutAlias] = columnValue
+                end
+            end
+
+            for foreignKeyName, v in pairs(belongedModels) do
+                if self.belongsTo ~= nil and self.belongsTo[foreignKeyName] ~= nil then
+
+                    local belongedClass = require("models."..self.belongsTo[foreignKeyName])
+                    local belongedModel = belongedClass(belongedModels[foreignKeyName])
+
+                    model[foreignKeyName] = belongedModel
+
+                end
+            end
+
+            if model.rowid ~= nil then
+                for recordsName, className in pairs(hasManyModels) do
+                    local foreignKey = self.name.."_id"
+                    local condition = {}
+                    condition[foreignKey] = model.rowid
+                    local hasManyClass = require("models."..self.hasMany[recordsName])
+                    local foreignRows = hasManyClass:where(condition):findAll()
+                    if foreignRows ~= nil and type(foreignRows) == "table" and #foreignRows > 0 then
+                        model[recordsName] = foreignRows
+                    else
+                        model[recordsName] = {}
+                    end
+                end
+            end
+            
+            
+            
+        end
+        local myClass = require("models."..self.name)
+        local myModel = myClass(model)
+        table.insert(models, myModel)
+    end
+    self:_closeDatabase(db)
+    
+    return models
+end
+
+function Model:_createTableSQL(model)
+    local db = sqlite3.open( path )
+    local result = false
+    if model.isTableCreated == nil or model.isTableCreated == false then
+
+      if model.columns == nil and model.name == nil then
+          error( "You should not call this method directly.")
+      end
+
+      local sql = Table:_generateSQL(model)
+      
+      if DEBUG == false then
+        local sqlStatus = db:exec( sql )
+        
+        if sqlStatus == 0 then
+            -- should not create a table everytime a new instance is created
+            local myClass = require("models."..model.name)
+            myClass.isTableCreated = true
+            return true
+        end
+      else
+        result = true
+      end
+
+    end
+    self:_closeDatabase(db)
+    return result
+end
+
+function Model:delete()
+    local db = sqlite3.open( path )
+    local generatedSql = Table:_generateSQLDelete(self)
+    local result = db:exec(generatedSql)
+    if result then
+        self.rowid = nil
+        return true
+    end
+    self:_closeDatabase(db)
+    return false
+end
+
+function Model:save()
+    local db = sqlite3.open( path )
+    local result = false
+    if self.rowid == nil then
+
+        local generatedSqlSave = Table:_generateSQLInsert(self)
+        local result = db:exec(generatedSqlSave)
+        print("opa")
+        if result == 0 then
+            --[[local sqlGetLastPK = "SELECT rowid FROM "..self.name.." ORDER BY ROWID DESC LIMIT 1"
+            for row in db:nrows(sqlGetLastPK) do
+                primaryKey = row.rowid
+            end]]
+            local primaryKey = db:last_insert_rowid()
+            if primaryKey ~= 0 then
+                self.rowid = primaryKey
+                result = true
+            else
+                result = false
+            end
+        end
+
+        result = false
+    else
+        local generatedSql = Table:_generateSQLUpdate(self)
+        local result = db:exec(generatedSql)
+        if result == 0 then
+            result = true
+        end
+
+        result = false
+    end
+    self:_closeDatabase()
+    return result
+end
+
+function Model:_closeDatabase(db)
+    if ( db and db:isopen() ) then
+            db:close()
+    end
+end
+
+function Model:_dropDatabase()
+    local lfs = require( "lfs" )
+    os.remove(path)
+    
+end
+
+return Model
---
+++ readme.md
@@ -0,0 +1,100 @@
+# Summary
+
+Corona SDK (http://www.coronalabs.com) lacks a ORM-like to manage SQLite database in a meaniful way. You have to write pure SQL and it is not good, also hard to mantain. This library has three main points:
+
+1. ORM syntax for database manipulation in Corona; 
+2. Based on Active record pattern (https://www.martinfowler.com/eaaCatalog/activeRecord.html);
+3. Support for object oriented paradigm (class, instances and inherintance)**
+
+** For that I make use of 30log library: https://github.com/Yonaba/30log
+
+# Usage
+
+Create a class that extends Model (our Active Record Pattern). Model is a base class which implements Save, Delete, Find all, Update and support for relationships (at the time one to one and one to many.)
+
+Example:
+
+```lua
+local Person = Model:extend("Person", { columns = {
+    age = {}, 
+    surname = { notNull=true }}}
+  )
+
+-- This is the constructor. 
+function Person:init(columns)
+    -- We are calling Model's (super) constructor. It is required.
+    Person.super:init(columns, self) 
+end
+```
+Now, just create an instance of Person and start using it:
+
+```lua
+local aPerson = Person({age=30, surname="Soares"})
+aPerson:save()
+```
+
+# Examples
+
+## Simple C.R.U.D.
+
+```lua
+-- Every database table must extends Model as it implements basic CRUD operations.
+local Person = Model:extend("Person", { columns = { -- database columns should be inside this table
+    age = {}, -- every key is a column in Person table.
+    surname = { notNull=true }}} -- you can configure not null like this.
+  )
+
+-- This is the constructor. 
+function Person:init(columns)
+    -- You can do anything you want, but you must call its parent constructor (Model)
+    Person.super:init(columns, self) 
+end
+
+-- Creating a Person instance.
+local aPerson = Person({age=30, surname="Soares"})
+aPerson:save() -- saves on database
+aPerson = 28
+aPerson:save() -- updates
+local persons = Person:findAll() -- Select * From Person. Returns a table with results.
+if #persons > 0 then
+    for i=1, #persons do
+        print(persons[i].age)
+        print(persons[i].rowid)
+    end
+end
+aPerson:delete() -- delete
+```
+
+## Support for relationships
+
+```lua
+local Animal = Model:extend("Animal", { columns = {
+    nickname = {}},
+    belongsTo={animalOwner="Person"}
+    }
+  )
+
+function Animal:init(columns)
+    Animal.super:init(columns, self)
+end
+
+local animalInstance = Animal({nickName="tob", animalOwner=aPerson})
+animalInstance:save()
+
+local animals = Animal:findAll()
+print(#animals)
+if #animals > 0 then
+    for i=1, #animals do
+        print(animals[i].aPerson.age) -- should print 28
+    end
+end
+```
+
+# License
+
+MIT.
+
+# Creator
+
+Leonardo Soares e Silva
+lsoaresesilva@gmail.com
